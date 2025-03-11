b4228eae375a5d99cfe8215e0edf13bc8e36ab61
glebchik_gg <gaben123001@gmail.com>
2025-03-11 13:13:12 +0300

    test this shit (in good meaning)

--- src/board.cpp
+++ src/board.cpp
@@ -5,9 +5,18 @@ namespace Leslie
 {
 	std::string to_str(bitboard board)
 	{
-		// TODO: implementation
-		uint64_t mask = 0xFF;
-		return "";
+		char result[72];
+		char* ptr = result;
+
+		for (int rank = 7; rank >= 0; --rank) {
+			for (int file = 7; file >= 0; --file) {
+				*ptr++ = (board & (1ULL << (rank * 8 + file))) ? '1' : '0';
+			}
+			*ptr++ = '\n';
+		}
+		*ptr = '\0';
+
+		return std::string(result);
 	}
 
 	bitboard Board::get_white_pawn_advances()
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,9 +4,21 @@
 
 TEST(general, to_str)
 {
-	constexpr std::string expected = "00000000\n10100101\n11111111\n00000000\n11110000\n00000000\n00000000\n11111111\n";
-	constexpr Leslie::bitboard row = 0b0000000010100101111111110000000011110000000000000000000011111111;
+	std::string expected = "00000000\n10100101\n11111111\n00000000\n11110000\n00000000\n00000000\n11111111\n";
+	Leslie::bitboard row = 0b0000000010100101111111110000000011110000000000000000000011111111;
 	EXPECT_EQ(expected, Leslie::to_str(row));
+}
+
+TEST(general, rank5)
+{
+	Leslie::bitboard expected = 0x000000FF00000000;
+	EXPECT_EQ(expected, Leslie::Rank5);
+}
+
+TEST(general, fileF)
+{
+	Leslie::bitboard expected = 0x0404040404040404;
+	EXPECT_EQ(expected, Leslie::FileF);
 }
 
 int main(int argc, char* argv[])
