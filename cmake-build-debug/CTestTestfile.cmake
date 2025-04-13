17ca38d87d3d110e41f8c7be1c0434b9b045c867
glebchik_gg <gaben123001@gmail.com>
2025-04-14 00:25:34 +0300

    clear kal

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Mi/LEARNING/Leslie-ChessEngine
-# Build directory: C:/Users/Mi/LEARNING/Leslie-ChessEngine/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[LeslieTests]=] "C:/Users/Mi/LEARNING/Leslie-ChessEngine/cmake-build-debug/Leslie_ChessEngine.exe")
-set_tests_properties([=[LeslieTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Mi/LEARNING/Leslie-ChessEngine/CMakeLists.txt;51;add_test;C:/Users/Mi/LEARNING/Leslie-ChessEngine/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
